b0b5755fab9815bf0d32b6696662896be2a1412a
Jordi Petchamé <jordi.petchame.guerrero@gmail.com>
2025-09-28 15:27:34 +0200

    source REMIX eliminado

--- src_REMIX/compute_logK_star_spec.f90
+++
@@ -1,8 +0,0 @@
-subroutine compute_logK_star(this,K)
-    use speciation_algebra_m, only: speciation_algebra_c
-    implicit none
-    class(speciation_algebra_c) :: this
-    real(kind=8), intent(in) :: K(:) ! eq csts
-   
-    this%logK_star=matmul(this%inv_Se_nc_2,log10(K))
-end subroutine
